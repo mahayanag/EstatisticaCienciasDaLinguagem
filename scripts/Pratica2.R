7efa788b74e89cc1b498fcae27f11dc0a47a48a5
Mahayana Godoy <mahayanagodoy@MacBook-Pro-de-Mahayana.local>
2020-12-15 08:54:37 -0300

    videos 4 e 5

---
+++ scripts/Pratica2.R
@@ -0,0 +1,34 @@
+### Referentes aos videos 4 e 5
+
+
+# Importe o conjunto de dados `alunos.csv` e tente fazer as seguintes tarefas:
+  
+# 1. Inspecione as colunas para saber como as variaveis estao codificadas;
+
+# 2. Transforme a variavel `genero` e a variavel `estimacao` em um fator e veja quantos niveis essas variaveis tem;
+
+# 3. Veja quantos valores unicos a variavel `irmaos` tem;
+
+# 4. Crie um subconjunto chamado `alunos.gato` que contenha observacoes das variaveis `ano`, `idade` e `genero` apenas dos alunos que tenham apenas gato como animal de estimacao. Nao se esqueca do `drop_levels()` ao fim do codigo!
+  
+# 5. Use a funcao `str()` para conferir se voce conseguiu criar o subconjunto `alunos.gatos` satisfatoriamente. 
+
+#Os dados presentes no arquivo `alunos.csv` foram coletados ao longo de quatro anos. Esse arquivo contem as respostas de alunos ingressantes no curso de Bacharelado em Estatistica da UFRN a um questionario aplicado durante o primeiro mes de aulas em cada ano. Cada linha representa um aluno e suas colunas sao:
+  
+# `ano`: ano de ingresso no curso
+# `genero`: masculino ou feminino
+# `idade`: em anos
+# `altura`: em cm
+# `peso`: em kg
+# `calcado`: numero do calcado
+# `irmaos`: numero de irmaos
+# `namorado`: se possui namorado(a) ou conjuge
+# `fumante`: fuma ou nao
+# `alcool`: numero de vezes que consome alcool semanalmente
+# `estimacao`: tipo de animal de estimacao
+# `exercicio`: numero de horas semanais de exercicio fisico
+# `enem`: nota obtida no ENEM
+# `sono`: horas diarias de sono
+# `ufrn`: opiniao sobre a estrutura da UFRN
+
+
---
+++ scripts/video5.R
@@ -0,0 +1,100 @@
+## CODIGO PARA CAPITULO 2
+
+
+# importar e inspecionar esplex
+
+esplex <- read.csv("dados/esplex.csv")
+
+str(esplex)
+
+
+# criar nova coluna metade_frq
+
+esplex$metade_frq <- esplex$frq/2
+
+head(esplex)
+
+
+# verificando valores unicos para variaveis
+
+unique(esplex$categoria)
+
+unique(esplex$num_caracteres)
+
+
+## erro em codigo: NULL
+
+levels(esplex$categoria)
+
+
+# mudando a variavel para codificar como categorica 
+
+esplex$categoria <- as.factor(esplex$categoria )
+
+# inspecionado a codificacao da variavel "categoria"
+
+str(esplex$categoria)
+
+
+# niveis da variavel "categoria"
+
+levels(esplex$categoria)
+
+
+# carregando pacotes
+
+library(tidyverse)
+
+
+
+# selecionando variaveis palavra, concretude, imageabilidade e categoria
+ 
+ esplex %>%
+   select(palavra, concretude, imageabilidade, categoria)
+
+# selecionando apenas palavras na categoria NOME ou VERBO
+
+esplex %>%
+  filter(categoria == "NOME" | categoria == "VERBO")
+
+
+# selecionando variaveis e filtrando observacoes
+
+esplex %>%
+  select(palavra, concretude, imageabilidade, categoria) %>%
+  filter(categoria == "NOME" | categoria == "VERBO") %>%
+  filter(concretude > 3)
+
+
+ # criando conjunto esplex2
+ 
+ esplex2 <- esplex %>%
+   select(palavra, concretude, imageabilidade, categoria) %>%
+   filter(categoria == "NOME" | categoria == "VERBO") %>%
+   filter(concretude > 3) %>%
+   droplevels()
+
+
+## codigo com erro
+
+esplex %>%
+  select(palavra, concretude) %>%
+  filter(categoria == "NOME" | categoria == "VERBO")
+
+
+# outra maneira de usar pipe, mantendo codigo na mesma linha
+
+ esplex %>% select(palavra, concretude, imageabilidade, categoria)
+ 
+
+# selecionando observcoes de palavras especificas
+ 
+ esplex %>%
+   filter(palavra == "manera" | palavra == "situación" | palavra == "problema" | palavra == "unión" | palavra == "político" | palavra == "madre")
+ 
+# criando vetor e usando %in%
+
+esplex %>%
+  filter(palavra %in% c("manera", "situación", "problema", "unión",  "político", "madre"))
+
+
